94042d6e7c778ce5a2dce811d58cdef054b0725c
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-03 05:38:01 -0500

    progress

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,20 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model')
+
+async function validateId (req, res, next) {
+    try {
+      const id = await Projects.get(req.params.id);
+      if (!id) {
+        res.status(404).json({ message: "project not found" });
+      } else {
+        req.id = id;
+        next();
+      }
+    } catch (err) {
+      res.status(500).json({ message: "uh oh problems finding project" });
+    }
+  }
+
+module.exports = {
+    validateId 
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,15 +1,74 @@
 // Write your "projects" router here!
 const express = require("express");
+const { validateId } = require('./projects-middleware')
 
 const router = express.Router();
-const Projects = require('./projects-model')
+const Projects = require("./projects-model");
 
-router.get("/", (req, res) => {
-    res.json([
-      { id: 1, username: "foo", password: "p123" },
-      { id: 2, username: "bar", password: "p456" },
-      { id: 3, username: "foobar", password: "p789" },
-    ]);
+router.get("/", async (req, res, next) => {
+  try {
+    const proj = await Projects.get();
+    res.json(proj);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", validateId, async (req, res, next) => {
+  try {
+    const proj = await Projects.get(req.params.id);
+    res.json(proj);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    Projects.get();
+
+    res.json(res);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/", async (req, res, next) => {
+  try {
+    Projects.get();
+
+    res.json(res);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/", async (req, res, next) => {
+  try {
+    Projects.get();
+
+    res.json(res);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    Projects.get();
+
+    res.json(res);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, res, req, next) => { //eslint-disable-line
+  res.status(err.status || 500).json({
+    customMessage: "Something tragic happened inside projects-router",
+    message: err.message,
+    stack: err.stack,
   });
+});
 
-  module.exports = router;
+module.exports = router;
