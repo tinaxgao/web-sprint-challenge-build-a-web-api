f1648ad1edcea8e4f3ee23ec75d37ff9c8db64f2
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-12-02 22:03:54 -0500

    progress

--- api/server.js
+++ api/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json());
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.get("/api/users", (req, res) => {
+    res.json([
+      { id: 1, username: "foo", password: "p123" },
+      { id: 2, username: "bar", password: "p456" },
+      { id: 3, username: "foobar", password: "p789" },
+    ]);
+  });
 
 module.exports = server;
--- index.js
+++ index.js
@@ -17,17 +17,11 @@ require("dotenv").config();
 const path = require("path");
 const express = require("express");
 
-const server = express();
-server.use(express.json());
+const server = require('./api/server')
+
 server.use(express.static(path.join(__dirname, "client/build")));
 
-server.get("/api/users", (req, res) => {
-  res.json([
-    { id: 1, username: "foo", password: "p123" },
-    { id: 2, username: "bar", password: "p456" },
-    { id: 3, username: "foobar", password: "p789" },
-  ]);
-});
+
 
 server.get(`*`, (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
